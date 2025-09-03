293afd48a6ca479b8715e486f1a24c6ff8d6cd94
Jaryllim98 <jaryl.lim@gmail.com>
2025-09-03 09:10:48 +0800

    console log

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
+
+console.log("Attempting to connect to Supabase URL:", process.env.REACT_APP_SUPABASE_URL);
+console.log("Is Supabase Anon Key loaded:", process.env.REACT_APP_SUPABASE_ANON_KEY? "Yes" : "No, it is NOT LOADED");
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
